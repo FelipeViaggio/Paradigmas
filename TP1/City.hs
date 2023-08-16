bc4f6d481f3455a8e006233cb8d58c343ccd6993
FelipeViaggio <fviaggio@udesa.edu.ar>
2023-08-16 13:10:56 -0300

    Co-authored-by: timomenceyra <timomenceyra@users.noreply.github.com> point y city

--- TP1/City.hs
+++ TP1/City.hs
@@ -6,5 +6,10 @@ import Point
 data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
+newC nombre ubicacion = Cit nombre ubicacion
+
 nameC :: City -> String
+nameC (Cit nombre ubicacion) = nombre
+
 distanceC :: City -> City -> Float
+distanceC (Cit nombre1 ubicacion1) (Cit nombre2 ubicacion2) = difP ubicacion1 ubicacion2
