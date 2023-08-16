aca3254ccbfae15643f0484e8f3e36ee95f812ad
FelipeViaggio <fviaggio@udesa.edu.ar>
2023-08-16 14:01:09 -0300

    termine quality y link

--- TP1/Link.hs
+++ TP1/Link.hs
@@ -7,7 +7,18 @@ import Quality
 data Link = Lin City City Quality deriving (Eq, Show)
 
 newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades distintas
+newL cityA cityB quality = Lin cityA cityB quality
+
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
+connectsL thisCity (Lin cityA cityB quality) | thisCity == cityA || thisCity == cityB = True
+                                             | otherwise = False
+
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
+linksL firstCity secondCity (Lin cityA cityB quality) | (firstCity == cityA && secondCity == cityB) || (firstCity == cityB && secondCity == cityA) = True
+                                                      | otherwise = False
+
 capacityL :: Link -> Int
+capacityL (Lin cityA cityB quality) = capacityQ quality
+
 delayL :: Link -> Float     -- la demora que sufre una conexion en este canal
+delayL (Lin cityA cityB  quality) = delayQ quality
--- TP1/Quality.hs
+++ TP1/Quality.hs
@@ -5,6 +5,10 @@ module Quality ( Quality, newQ, capacityQ, delayQ )
 data Quality = Qua String Int Float deriving (Eq, Show)
 
 newQ :: String -> Int -> Float -> Quality
-newQ 
+newQ name capacity delay = Qua name capacity delay
+
 capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
+capacityQ (Qua name capacity delay) = capacity
+
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
+delayQ (Qua name capacity delay) = delay
