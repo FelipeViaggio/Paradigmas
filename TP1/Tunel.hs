eea361cde20845cf7781f35d33d62d0485c306e7
timomenceyra <tmenceyra@udesa.edu.ar>
2023-08-16 16:19:45 -0300

    push

--- TP1/Tunel.hs
+++ TP1/Tunel.hs
@@ -1,9 +1,18 @@
 module Tunel ( Tunel, newT, connectsT, usesT, delayT )
    where
 
+import City
+import Link
+
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
+newT (link:listaLinks) = Tun (link:listaLinks)
+
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
+-- connectsT firstCity secondCity (Tun (link:listaLinks)) | 
+
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
+usesT (Lin cityA cityB quality) (Tun link:listaLinks) = [y | elem y listaLinks]
+
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
