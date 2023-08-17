e7936da3bc7e777275fe3c05a2fd4d425a36610a
timomenceyra <tmenceyra@udesa.edu.ar>
2023-08-17 18:02:14 -0300

    push

--- TP1/Tunel.hs
+++ TP1/Tunel.hs
@@ -11,7 +11,8 @@ newT :: [Link] -> Tunel
 newT (link:listaLinks) = Tun (link:listaLinks)
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-
+connectsT city1 city2 (Tun links) = (connectsL city1 (head links)) && (connectsL city2 (last links)) || 
+                                    (connectsL city2 (head links)) && (connectsL city1 (last links))
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 usesT link (Tun listaLinks) = elem link listaLinks
