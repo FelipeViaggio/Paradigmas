7e250a77880c173708b311e9e7c0cef14499c57c
FelipeViaggio <fviaggio@udesa.edu.ar>
2023-08-23 11:31:18 -0300

    cc

--- TP1/Link.hs
+++ TP1/Link.hs
@@ -1,6 +1,7 @@
 module Link ( Link, newL, linksL, connectsL, capacityL, delayL )
    where
 
+import Point
 import City
 import Quality
 
@@ -10,8 +11,7 @@ newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades dis
 newL cityA cityB quality = Lin cityA cityB quality
 
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
-connectsL thisCity (Lin cityA cityB quality) | thisCity == cityA || thisCity == cityB = True
-                                             | otherwise = False
+connectsL thisCity (Lin cityA cityB quality) = thisCity == cityA || thisCity == cityB 
 
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
 linksL firstCity secondCity (Lin cityA cityB quality) | (firstCity == cityA && secondCity == cityB) || (firstCity == cityB && secondCity == cityA) = True
