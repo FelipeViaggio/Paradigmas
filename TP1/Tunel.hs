37e89a9afb59d00244e367f84bfaf7d80efcd1c0
FelipeViaggio <fviaggio@udesa.edu.ar>
2023-08-23 12:26:21 -0300

    Co-authored-by: timomenceyra <timomenceyra@users.noreply.github.com>commit

--- TP1/Tunel.hs
+++ TP1/Tunel.hs
@@ -11,12 +11,16 @@ data Tunel = Tun [Link] deriving (Eq, Show)
 newT :: [Link] -> Tunel
 newT = Tun
 
+isFirst :: City -> Tunel -> Bool
+isFirst currentCity (Tun links) = connectsL currentCity (head (take 2 links)) && not (connectsL currentCity (last (take 2 links)))
+
+isLast :: City -> Tunel -> Bool
+isLast currentCity (Tun links) = not (connectsL currentCity (head (drop (length links - 2) links))) && connectsL currentCity (last (drop (length links -2) links))
+
 areExtremes :: City -> City -> Tunel -> Bool
 areExtremes city1 city2 (Tun links) | length links == 1 = linksL city1 city2 (head links)
-                                    | length links > 1 = (connectsL city1 (head (take 2 links)) && not (connectsL city1 (last (take 2 links)))) && 
-                                                         (not (connectsL city2 (head (drop (length links - 2) links))) && connectsL city2 (last (drop (length links -2) links))) ||
-                                                         (connectsL city2 (head (take 2 links)) && not (connectsL city2 (last (take 2 links)))) && 
-                                                         (not (connectsL city1 (head (drop (length links - 2) links))) && connectsL city1 (last (drop (length links -2) links)))
+                                    | length links > 1 = isFirst city1 (Tun links) && isLast city2 (Tun links) ||
+                                                         isFirst city2 (Tun links) && isLast city1 (Tun links)
 
 connectsT :: City -> City -> Tunel -> Bool -- indica si este tunel conecta estas dos ciudades distintas
 connectsT city1 city2 (Tun links) = areExtremes city1 city2 (Tun links)
@@ -28,5 +32,4 @@ usesT link (Tun linkslist) = True-- | elem link linkslist || reverseLink elem li
                            
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
-delayT (Tun []) = 0
-delayT (Tun (link:listaLinks)) = delayL link + delayT (Tun listaLinks)
+delayT (Tun linkslist) = sum (map delayL linkslist)
