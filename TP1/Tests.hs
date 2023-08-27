6377b2685322101ead811cd18bf49f60f904b660
FelipeViaggio <fviaggio@udesa.edu.ar>
2023-08-27 15:48:25 -0300

    cc

--- TP1/Tests.hs
+++ TP1/Tests.hs
@@ -45,4 +45,24 @@ lista = [
          availableCapacityForR region madrid berlin == 0, availableCapacityForR region berlin bsas == 1
         ]
 
+
+testF :: Show a => a -> Bool
+testF action = unsafePerformIO $ do
+    result <- tryJust isException (evaluate action)
+    return $ case result of
+        Left _ -> True
+        Right _ -> False
+    where
+        isException :: SomeException -> Maybe ()
+        isException _ = Just ()
+
+result :: Int -> Int
+result x | x > 5 = 4
+         | otherwise = error "hey"
+
+-- ahora pueden evaluar
+t = [ testF (result 3 ),
+      testF (result 8 ) ]
+
+      
 main = print lista
